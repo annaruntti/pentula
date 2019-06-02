25c3f4c7631dfbaa528cb72f2dd63a5ce2b646e1
Anna Tiala <anruntti@gmail.com>
2019-06-02 15:49:39 +0300

    connecting front to backend

--- src/components/FormAddDog/FormAddDog.jsx
+++ src/components/FormAddDog/FormAddDog.jsx
@@ -3,89 +3,158 @@ import React from "react";
 import "./FormAddDog.scss";
 
 class FormAddDog extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      name: "",
-      officialname: "",
-      bdate: "",
-      sex: [],
-      heat: ""
+      // nimi: "",
+      omat_koirat: []
     };
 
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleOfficialNameChange = this.handleOfficialNameChange.bind(this);
-    this.handleBdateChange = this.handleBdateChange.bind(this);
-    this.handleSexChange = this.handleSexChange.bind(this);
+    // this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
+  // Make AJAX calls here
 
-  handleOfficialNameChange(event) {
-    this.setState({ officialname: event.target.value });
-  }
-
-  handleBdateChange(event) {
-    this.setState({ bdate: event.target.value });
-  }
-
-  handleSexChange(event) {
-    this.setState({
-      sex: event.target.value
+  componentDidMount() {
+    var that = this;
+    fetch("http://localhost:8000/api/omat-koirat").then(function(response) {
+      response.json().then(function(data) {
+        that.setState({
+          omat_koirat: data
+        });
+      });
     });
   }
 
-  handleHeatChange(event) {
-    this.setState({
-      heat: event.target.value
+  addDog(event) {
+    var that = this;
+    event.preventDefault();
+    let post_data = {
+      virallinen_nimi: this.refs.virallinen_nimi.value,
+      kutsumanimi: this.refs.kutsumanimi.value,
+      syntymaaika: this.refs.syntymaaika.value,
+      sukupuoli: this.refs.sukupuoli.value,
+      // accept: this.refs.accept.value,
+      user_id: Math.floor(Math.random() * 1000) + 4
+    };
+    var request = new Request("http://localhost:8000/api/uusi-koira", {
+      method: "POST",
+      headers: new Headers({ "Content-Type": "application/json" }),
+      body: JSON.stringify(post_data)
     });
+
+    let omat_koirat = that.state.omat_koirat;
+    omat_koirat.push(post_data);
+    console.log(omat_koirat);
+    that.setState({
+      omat_koirat: omat_koirat
+    });
+
+    // xmlhttprequest()
+    fetch(request)
+      .then(function(response) {
+        response.json().then(function(data) {});
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
   }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     nimi: "",
+  //     virallinen_nimi: "",
+  //     syntymaaika: "",
+  //     sukupuoli: [],
+  //     juoksu: ""
+  //   };
+
+  //   this.handleNameChange = this.handleNameChange.bind(this);
+  //   this.handleOfficialNameChange = this.handleOfficialNameChange.bind(this);
+  //   this.handleBdateChange = this.handleBdateChange.bind(this);
+  //   this.handleSexChange = this.handleSexChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // handleNameChange(event) {
+  //   this.setState({ nimi: event.target.value });
+  // }
+
+  // handleOfficialNameChange(event) {
+  //   this.setState({ virallinen_nimi: event.target.value });
+  // }
+
+  // handleBdateChange(event) {
+  //   this.setState({ syntymaaika: event.target.value });
+  // }
+
+  // handleSexChange(event) {
+  //   this.setState({
+  //     sukupuoli: event.target.value
+  //   });
+  // }
+
+  // handleHeatChange(event) {
+  //   this.setState({
+  //     juoksu: event.target.value
+  //   });
+  // }
 
   handleSubmit(event) {
-    alert("Uusi koira lisätty: " + this.state.name);
+    alert("Uusi koira lisätty: " + this.state.kutsumanimi);
     event.preventDefault();
   }
 
   render() {
+    let omat_koirat = this.state.omat_koirat;
     return (
-      <form onSubmit={this.handleSubmit}>
+      // <form onSubmit={this.handleSubmit}>
+      <form ref="postForm" onSubmit={this.onFormSubmit}>
         <div className="input-area">
           <label>
             Koirasi kutsumanimi:
             <input
-              id="name"
-              name="name"
+              id="kutsumanimi"
+              name="kutsumanimi"
+              ref="kutsumanimi"
               type="text"
-              value={this.state.name}
-              onChange={this.handleNameChange}
+              // value={this.state.nimi}
+              // onChange={this.handleNameChange}
             />
           </label>
           <label>
             Koirasi virallinen nimi:
             <input
-              id="officialname"
-              name="officialname"
+              id="virallinen_nimi"
+              name="virallinen_nimi"
               type="text"
-              value={this.state.officialname}
-              onChange={this.handleOfficialNameChange}
+              ref="virallinen_nimi"
+              // value={this.state.virallinen_nimi}
+              // onChange={this.handleOfficialNameChange}
             />
           </label>
           <label>
             Koirasi syntymäaika:
             <input
-              id="bdate"
-              name="bdate"
+              id="syntymaaika"
+              name="syntymaaika"
               type="text"
-              value={this.state.bdate}
-              onChange={this.handleBdateChange}
+              ref="syntymaaika"
+              // value={this.state.syntymaaika}
+              // onChange={this.handleBdateChange}
             />
           </label>
           <label>
             <span className="select-label">Koirasi sukupuoli:</span>
-            <select value={this.state.sex} onChange={this.handleSexChange}>
+            <select
+              id="sukupuoli"
+              name="sukupuoli"
+              ref="sukupuoli"
+              // value={this.state.sukupuoli}
+              // onChange={this.handleSexChange}
+            >
               <option value="">Valitse sukupuoli</option>
               <option value="uros">Uros</option>
               <option value="narttu">Narttu</option>
@@ -93,7 +162,13 @@ class FormAddDog extends React.Component {
           </label>
         </div>
         <div className="btn-area">
-          <button className="submit-btn" type="submit" value="Submit">
+          {/* <button className="submit-btn" type="submit" value="Submit"> */}
+          <button
+            className="submit-btn"
+            onClick={this.addDog.bind(this)}
+            type="submit"
+            value="Submit"
+          >
             Lähetä
           </button>
         </div>
@@ -101,21 +176,5 @@ class FormAddDog extends React.Component {
     );
   }
 }
-
-// function FormAddDog() {
-//   return (
-//     <form className="form-add-new">
-//       <label>
-//         Koirasi kutsumanimi:
-//         <input type="text" name="name" />
-//       </label>
-//       <label>
-//         Koirasi virallinen nimi:
-//         <input type="text" name="official-name" />
-//       </label>
-//       <input type="submit" value="Submit" />
-//     </form>
-//   );
-// }
 
 export default FormAddDog;
