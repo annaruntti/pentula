fa2796b44c95d8d7023c219e63fd692c183d36e6
Anna Tiala <anruntti@gmail.com>
2020-07-08 14:07:26 +0300

    remove commented rows

--- src/components/FormAddLitter/FormAddLitter.jsx
+++ src/components/FormAddLitter/FormAddLitter.jsx
@@ -1,27 +1,22 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 import "./FormAddLitter.scss";
 
 class FormAddLitter extends React.Component {
   constructor() {
     super();
     this.state = {
-      // nimi: "",
-      omat_pentueet: []
+      omat_pentueet: [],
     };
 
-    // this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // Make AJAX calls here
-
   componentDidMount() {
     const that = this;
-    fetch("http://localhost:8000/api/omat-pentueet").then(function(response) {
-      response.json().then(function(data) {
+    fetch("http://localhost:8000/api/omat-pentueet").then(function (response) {
+      response.json().then(function (data) {
         that.setState({
-          omat_pentueet: data
+          omat_pentueet: data,
         });
       });
     });
@@ -36,27 +31,27 @@ class FormAddLitter extends React.Component {
       emon_nimi: this.refs.emon_nimi.value,
       isan_nimi: this.refs.isan_nimi.value,
       lisatiedot: this.refs.lisatiedot.value,
-      litter_id: Math.floor(Math.random() * 1000) + 4
+      litter_id: Math.floor(Math.random() * 1000) + 4,
     };
     const request = new Request("http://localhost:8000/api/uusi-pentue", {
       method: "POST",
       headers: new Headers({ "Content-Type": "application/json" }),
-      body: JSON.stringify(post_data)
+      body: JSON.stringify(post_data),
     });
 
     const omat_pentueet = that.state.omat_pentueet;
     omat_pentueet.push(post_data);
     console.log(omat_pentueet);
     that.setState({
-      omat_pentueet: omat_pentueet
+      omat_pentueet: omat_pentueet,
     });
 
     // xmlhttprequest()
     fetch(request)
-      .then(function(response) {
-        response.json().then(function(data) {});
+      .then(function (response) {
+        response.json().then(function (data) {});
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
       });
   }
@@ -69,7 +64,6 @@ class FormAddLitter extends React.Component {
   render() {
     const omat_pentueet = this.state.omat_pentueet;
     return (
-      // <form onSubmit={this.handleSubmit}>
       <form ref="postForm" onSubmit={this.onFormSubmit}>
         <div className="input-area">
           <label>
@@ -79,8 +73,6 @@ class FormAddLitter extends React.Component {
               name="syntymaaika"
               type="text"
               ref="syntymaaika"
-              // value={this.state.syntymaaika}
-              // onChange={this.handleBdateChange}
             />
           </label>
           <label>
@@ -91,8 +83,6 @@ class FormAddLitter extends React.Component {
               ref="pentueen_nimi"
               type="text"
               placeholder="Esim. A-pentue"
-              // value={this.state.nimi}
-              // onChange={this.handleNameChange}
             />
           </label>
           <label>
@@ -102,8 +92,6 @@ class FormAddLitter extends React.Component {
               name="emon_nimi"
               type="text"
               ref="emon_nimi"
-              // value={this.state.virallinen_nimi}
-              // onChange={this.handleOfficialNameChange}
             />
           </label>
           <label>
@@ -113,8 +101,6 @@ class FormAddLitter extends React.Component {
               name="isan_nimi"
               type="text"
               ref="isan_nimi"
-              // value={this.state.virallinen_nimi}
-              // onChange={this.handleOfficialNameChange}
             />
           </label>
           <label>
@@ -127,14 +113,13 @@ class FormAddLitter extends React.Component {
               type="text"
               ref="lisatiedot"
               placeholder="Esim. synnytyksen alkamis- ja loppumiskellonaika yms."
-              // value={this.state.virallinen_nimi}
-              // onChange={this.handleOfficialNameChange}
             />
           </label>
           <div className="flex-wrapper">
             <label className="form-item-left">
               Pentujen
-              <br /> lukumäärä:
+              <br />
+              lukumäärä:
             </label>
             <div className="form-item-right">
               <input type="text" id="pentu_lkm" />
@@ -143,7 +128,6 @@ class FormAddLitter extends React.Component {
           </div>
         </div>
         <div className="btn-area">
-          {/* <button className="submit-btn" type="submit" value="Submit"> */}
           <button
             className="submit-btn"
             onClick={this.addLitter.bind(this)}
